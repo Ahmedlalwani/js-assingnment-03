d52ba72f82cef91077528e7e086949ac9340fc35
ahmed.ismail.lalwani@gmail.com <ahmed.ismail.lalwani@gmail.com>
2023-09-15 23:36:56 +0500

    hello world

---
+++ app.js
@@ -0,0 +1,88 @@
+// Q1 answer
+
+// let text7;
+// let text2 = prompt("Enter any value or alphabet")
+
+// if (text2 >= "A" && text2 <= "Z")
+//           document.write(
+//           " is an UpperCase character <br>");
+//         else if (text2 >= "a" && text2 <= "z")
+//           document.write(
+//           " is an LowerCase character <br>");
+//         else document.write(
+//         " is a number <br>");
+
+//  Q2 answer
+
+//  var num1, num2;
+//  num1 = window.prompt("Input the First integer", "0");
+//         num2 = window.prompt("Input the second integer", "0");
+                                                         
+//  if(parseInt(num1, 10) > parseInt(num2, 10)) 
+//  { console.log("The larger of "+ num1+ " and "+ num2+ " is "+ num1+ ".");
+//           }   
+//  else if(parseInt(num2, 10) > parseInt(num1, 10)) 
+// {console.log("The larger of "+ num1+" and "+ num2+ " is "+ num2+ ".");
+//  }                  
+//  else {console.log("The values "+ num1+ " and "+num2+ " are equal.");
+//  }
+
+//  Q3 answer
+
+//  let text8;
+//  let text3 = prompt("Enter any digit value ")
+             
+//  if (text3 > 1 )
+// document.writeln(" is positive number <br>");
+//  else if (tevowelst3 < -1)
+//  document.writeln(" the value is negtive <br>");
+//  else 
+//  document.writeln(" the value is zero <br>");
+ 
+//  Q4 answer
+
+
+// let text9;
+
+// let vowels = prompt("Enter any alphabet")
+
+// if (vowels == 'a' || vowels == 'e' || vowels == 'i' ||
+// vowels == 'o' || vowels == 'u')
+//           document.write(" this is a vowel <br>");
+//         else 
+//           document.write(" this is not a vowel <br>");
+
+// Q5 answer 
+
+// let text;
+// let email = prompt("Enter your email")
+// let password = prompt("enter your password")
+// if (email == "abc@gmail.com"&&password =="hello")
+//           document.write("Correct! The password you entered matches the original password<br>");
+//         else 
+//           document.write("Incorrect password<br>");
+
+//   Q6 answer
+
+//  var greeting;
+//  var hour = 13;
+//  if (hour < 18) {
+//  greeting = "Good day";
+//  }
+//  else{
+//  greeting = "Good evening";
+//  }
+          
+//  Q7 answer
+          
+//  let time2;
+//  let time = prompt("Enter any time 4 digit :1200")
+          
+//  if (time >= 0000 && time <=1200)
+//  document.write(" Good Morning <br>");
+//  else if (time >= 1200 && time <= 1700)
+//  document.write(" Good Afternoon  <br>");
+//  else if (time >= 1700 && time <= 2100)
+//  document.write(" Good Evening <br>")
+//  else document.write(" Good Night <br>");
+                 
